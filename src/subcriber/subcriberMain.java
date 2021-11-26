749f4e9fbb57c6fed6d7df6d6749b391009e502e
tdtoan <ductoan2732000@gmail.com>
2021-11-26 22:10:45 +0700

    branch tdtoan

--- src/subcriber/subcriberMain.java
+++ src/subcriber/subcriberMain.java
@@ -1,9 +1,16 @@
 package subcriber;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class subcriberMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse("{\"topic\": \"John\"}");
 
+        System.out.println(json.get("topic"));
     }
 
 }
